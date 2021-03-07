a7afe85daf54f77893d41ce719de1647e69a8a0c
MizukiToyota <mtoyota@system-renovate.co.jp>
2021-03-07 20:34:28 +0900

    ホーム画面ファイル作成

---
+++ AgeDeterminationApp/AgeDeterminationApp/Models/Home/HomeModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeModel.swift
+//  AgeDeterminationApp
+//
+//  Created by Training on 2021/03/07.
+//  Copyright © 2021 training. All rights reserved.
+//
+
+import UIKit
+
+class HomeModel: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AgeDeterminationApp/AgeDeterminationApp/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  AgeDeterminationApp
+//
+//  Created by Training on 2021/03/07.
+//  Copyright © 2021 training. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
